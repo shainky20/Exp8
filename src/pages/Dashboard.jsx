5f637f7b678536d08b78a442d308b2eb4e71680e
shainky20 <shainkysoni47@gmail.com>
2026-04-14 08:29:27 +0530

    Add files via upload

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h2>Welcome to Dashboard</h2>
+      <p>You are logged in successfully ✅</p>
+    </div>
+  );
+}
